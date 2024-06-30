7312d945e8892d38393c4795c3fe9b1b86d6ebfe
arvin <arvinnemati95@gmail.com>
2024-06-30 09:45:21 +0300

    capitalize first letter

---
+++ capitalizeFirst.js
@@ -0,0 +1,7 @@
+// Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+
+function capitalizeFirst(arr) {
+    if (arr.length == 0) return [];
+    const capitalized =  arr[0][0].toUpperCase() + arr[0].slice(1)
+    return [capitalized].concat(capitalizeFirst(arr.slice(1)))
+  }
