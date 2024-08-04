6a9ed728b61b23bc4ec4b3f6a05eeb166c7451e0
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-04 09:24:05 +0330

    Is it an Int?

---
+++ isInteger.js
@@ -0,0 +1,17 @@
+// A square of squares
+
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+// Task
+
+// Given an integral number, determine if it's a square number:
+
+
+var isSquare = function(n){
+    //P integer, always,
+    //R return true or false
+    //P
+      //
+    return Number.isInteger(Math.sqrt(n))
+  }
