36f01d42908c745cdd0624a2bc319832c9b6aa20
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-16 11:22:58 +0330

    factorial

--- main.js
+++ main.js
@@ -26,3 +26,17 @@ function flattenAndSort(array) {
         return a / b;
     }
   }
+
+
+//Your task is to write function factorial.
+
+function factorial(n){
+  let sum = 1;
+   for (let i = 1; i <= n; i++){
+     sum = sum * i;
+   }
+   return sum; 
+ 
+ }
+
+
