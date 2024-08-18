507ce06b763132db4ac92e349c316e4865b54713
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-18 21:22:10 +0330

    anagram challenge

--- main.js
+++ main.js
@@ -58,3 +58,14 @@ function getCount(str) {
   })
   return sum
 }
+
+//n anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+//Note: anagrams are case insensitive
+//Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+var isAnagram = function(test, original) {
+  var t = test.toLowerCase().split('').sort().join('');
+  var o = original.toLowerCase().split('').sort().join('');
+  return (t==o)?true:false;
+};
+
