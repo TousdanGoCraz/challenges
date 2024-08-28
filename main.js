e4ec02be3f1b11a46d8f07824204cdb22fd525aa
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-28 10:32:36 +0330

    CSV format

--- main.js
+++ main.js
@@ -151,4 +151,9 @@ function adjacentElementsProduct(array) {
   return Math.max(...newArr)
 }
 
+//Create a function that returns the CSV representation of a two-dimensional numeric array.
+function toCsvText(array) {
+   return array.join('\n');
+}
+
 
