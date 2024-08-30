6629670fab7dae081a23c9ee03995f036d9c845f
Arvin Nemati <arvinnemati95@gmail.com>
2024-08-30 12:59:15 +0330

    converted string to number in array

---
+++ stringToNum.js
@@ -0,0 +1,4 @@
+//Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+//ie:["1", "2", "3"] to [1, 2, 3]
+
+const toNumberArray = stringarray => stringarray.map(Number);
