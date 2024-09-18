abfcf48d3b31cd43ca62987112a7c044bdbe903b
Arvin Nemati <arvinnemati95@gmail.com>
2024-09-18 21:18:43 +0330

    add binary

---
+++ binaryAddition.js
@@ -0,0 +1,11 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+
+function addBinary(a,b) {
+    //P numbers, +, - , no floats, only numbers
+    //R sum of numbers, in binary
+    //p sum the numbers, parse as binary
+    return (a + b).toString(2)
+  }
