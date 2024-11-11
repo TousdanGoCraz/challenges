5b097f2e56b8d65e20e427d70160e912623836fd
arvin <arvinnemati95@gmail.com>
2024-11-11 19:04:24 +0300

    edited

--- insertionSort.js
+++ insertionSort.js
@@ -31,39 +31,41 @@
 
 
 
-let arr = [34, 8, 64, 51, 32, 21]
 // function insertSort(arr){
-//     for (let i = 1; i < arr.length;i++){
-//         let key = arr[i]
-//         let j = i - 1
+    //     for (let i = 1; i < arr.length;i++){
+        //         let key = arr[i]
+        //         let j = i - 1
+        
+        //         while (arr[j] > key && j >= 0){
+            //             arr[j + 1] = arr[j]
+            //             j--
+            //         }
+            //         arr[j+ 1] = key
+            //     }
+            //     return arr
+            // }
+            
+            
+            
+            
+            
+            
+            function insertionSort(arr){
+                
+                for (let i = 1; i < arr.length;i++){
+                    let key = arr[i]
+                    let j = i - 1
+                    
+                    while (key < arr[j] && j >= 0){
+                        arr[j + 1] = arr[j]
+                        j--
+                    }
+                    
+                    arr[j + 1] = key
+                }
+                
+                return arr
+            }
 
-//         while (arr[j] > key && j >= 0){
-//             arr[j + 1] = arr[j]
-//             j--
-//         }
-//         arr[j+ 1] = key
-//     }
-//     return arr
-// }
-
-// console.log(insertSort(arr))
-
-
-
-
-
-function insertionSort(arr){
-    
-    for (let i = 1; i < arr.length;i++){
-    let key = arr[i]
-    let j = i - 1
-
-    while (key < arr[j] && j >= 0){
-        arr[j + 1] = arr[j]
-        j--
-    }
-
-    arr[j + 1] = key
-    }
-
-}
+let arr = [34, 8, 64, 51, 32, 21]
+console.log(insertionSort(arr))
