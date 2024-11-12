186977d230d5682a0890eb5dcab5f62b08b252d3
arvin <arvinnemati95@gmail.com>
2024-11-12 09:45:21 +0300

    filter unique

---
+++ countUniqueVals.js
@@ -0,0 +1,9 @@
+function countUniqueValues(arr){
+    return arr.filter((x,i) => arr.indexOf(x) == arr.lastIndexOf(x))
+}
+
+
+console.log(countUniqueValues([1,1,1,1,1,2])) // 2
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-2,-1,-1,0,1]) // 4
