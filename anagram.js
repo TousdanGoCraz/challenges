f21ce78a4614c8b48f802e8e29338ff3e4521a7b
arvin <arvinnemati95@gmail.com>
2024-11-15 09:45:21 +0300

    anagram

--- anagram.js
+++ anagram.js
@@ -1,25 +1,20 @@
 function isAnagram(str1,str2){
     if (str1.length != str2.length) return false;
-    let freq1 = {}
-    let freq2 = {}
+    let list = {}
     for(let val of str1){
-        freq1[val] = (freq1[val] || 0 ) + 1
+        list[val] ? list[val]++ : list[val] = 1
     }
-    for(let val of str2){
-        freq2[val] = (freq2[val] || 0) + 1
-    }
+    console.log(list)
 
-    for (let key in freq1){
-        if (!(key in freq2)){
+    for (let val of str2){
+        if (!list[val]){
             return false
-        }
-        if (freq2[key] !== freq1[key]){
-            return false
+        }else{
+            list[val--]
         }
     }
-    console.log(freq1)
-    console.log(freq2)
     return true
+
 }
 
 console.log(isAnagram('temu','metu'))
