aa5cd3d0a36582c5ce08e009971b04281a51a783
arvin <arvinnemati95@gmail.com>
2024-11-17 19:56:14 +0300

    Frequency counter

---
+++ some.js
@@ -0,0 +1,24 @@
+function some(arr1,arr2){
+    if (arr1.length != arr2.length) return false
+
+    let freqCount1 = []
+    let freqCount2 = []
+    for(let val of arr1){
+        freqCount1[val] = (freqCount1[val] || 0) + 1
+
+    }
+    for(let val of arr2) {
+        freqCount2[val] = (freqCount2[val] || 0) +1
+    }
+    for (let key of freqCount1){
+        if (!(key ** 2 in freqCount2)){
+            return false
+        }
+        if (freqCount2[key ** 2] !== freqCount1[key]){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(some([1,2,1],[4,4,1]))
