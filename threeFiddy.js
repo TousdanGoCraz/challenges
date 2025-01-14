21e86027315ed90205a2b549425505b8731dff35
arvin <arvinnemati95@gmail.com>
2025-01-14 09:45:21 +0300

    three fiddy

---
+++ threeFiddy.js
@@ -0,0 +1,9 @@
+// It was then that you realize the musician was a 400 foot tall beast from the paleolithic era! The Loch Ness Monster almost tricked you!
+
+// There are only 2 guaranteed ways to tell if you are speaking to The Loch Ness Monster: A) it is a 400 foot tall beast from the paleolithic era; B) it will ask you for tree fiddy.
+
+// Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty". Your function should return true if you're talking with The Loch Ness Moster, false otherwise. 
+
+function isLochNessMonster(s) {
+    return s.includes('three fifty') || s.includes('3.50') || s.includes('tree fiddy')
+  }
