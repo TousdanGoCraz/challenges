63098db072dad38d8d92e0ffc47366591f7128e4
arvin <arvinnemati95@gmail.com>
2025-01-16 09:45:21 +0300

    true or false

---
+++ trueOrFalse.js
@@ -0,0 +1,5 @@
+// In javascript, Boolean object only has two value, true or false. Use ===, ==, >, <, >=, <= between two variables(or constant), the result of such an expression is a bool value. The operation between the two bool values can be used with logical operators:&&, ||, ! or some bitwise operators:&, |, ^
+
+function trueOrFalse(val){
+    return val ? 'true' : 'false'
+  }
